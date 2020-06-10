4c2233193e4231023ab4aaca155f7041591d5f93
Eling Meyatmaja <eling.atma@gmail.com>
2020-06-10 15:35:30 +0700

    add chat evet to server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,6 +8,9 @@ const Chat = ({ location }) =>{
 
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
     const ENDPOINT = 'localhost:5000'
 
     useEffect(() => {
@@ -23,9 +26,40 @@ const Chat = ({ location }) =>{
         })
         return () => {
             socket.emit('disconnect')
+
             socket.off()
         }
     },[ENDPOINT, location.search])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+
+    console.log(message, messages)
+    // function for sending message
+
+    return(
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event): null}
+                />
+            </div>
+        </div>
+    )
 
     return(
         <h1>Chat</h1>
